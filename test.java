26adbb702c9940c3ee7daf4b3cc60aea212146d3
FunmiOjoo <you@techwithfunmiojo@gmail.com>
2024-08-28 12:58:11 +0000

    test.java updated

--- test.java
+++ test.java
@@ -2,3 +2,7 @@ Testing by QA team
 Testing web apps before deployment
   running unit testing
   
+functional testing
+functional testing development
+functional testing development
+functional testing
